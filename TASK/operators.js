6e4c07d4d26c4ebfc536d970c76a8f2187397eed
srivalliravula1101-dev <srivalliravula1101@gmail.com>
2026-07-07 10:52:43 +0530

    Add files via upload

---
+++ TASK/operators.js
@@ -0,0 +1,49 @@
+// Arthematic operators:
+let name = "uma";
+console.log("Arthematic operators");
+console.log("Addition", 10 + 20);
+console.log("Subraction", 10 - 20);
+console.log("Multiplication", 10 * 20);
+console.log("Division", 10 / 20);
+console.log("Modulo", 10 % 20);
+console.log("Power", 10 ** 20);
+console.log(name);
+
+// assignment operators:
+// let name = "Uma";
+// console.log('My name is ${name}');//
+
+let a = 10;
+a += 20;
+console.log("a", a); //
+
+// Relational operators:
+console.log(10 === "10");
+console.log(10 !== "10"); //
+
+// logical operators:
+console.log(true && true);
+console.log(false || false);
+console.log(!false); //
+
+// bitwise:
+console.log(5 & 3);
+console.log(5 | 3);
+console.log(~5); //
+
+// unary operator:
+a = 10;
+a++;
+console.log(a);
+a = 8;
+a--;
+console.log(a);
+a = 12;
+console.log(++a);
+c = "5";
+console.log(typeof c);
+console.log(typeof +c); //
+
+// ternary:
+name = 10 < 0 ? "srivalli" : "uma";
+console.log(name); //
